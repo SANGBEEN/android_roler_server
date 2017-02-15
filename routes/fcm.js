bde0de909e9152c0b0df8e80d1de79cf084568c1
henry <calren00@gmail.com>
2017-02-16 01:26:15 +0900

    add fcm/push api

--- routes/fcm.js
+++ routes/fcm.js
@@ -18,4 +18,48 @@ router.put('/register', function(req, res){
   });
 });
 
+router.get('/push', function(req, res, next){
+  var dt = new Date();
+  var d = dt.toFormat('YYYY-MM-DD');
+  db.query('select s.user_id, u.id, u.token, s.content, s.starttime, s.date from schedule s inner JOIN user u on s.user_id = ? and u.id=? and date = ? order by starttime asc',[req.query.user_id, req.query.id, d], function(error, cursor){
+    if (error){
+      res.status(500).json({result : error});
+    }
+    else {
+      if(cursor.length>0){
+        var contents=[];
+        for(var i=0;i<cursor.length;i++){
+          contents.push({content:cursor[i].content});
+        }
+        var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+            to: cursor[0].token,
+            collapse_key: 'schedule',
+
+            notification: {
+                title: '오늘 해야 할 일',
+                body: contents
+            },
+
+            data: {  //you can send only notification or only data(or include both)
+                my_key: 'my value',
+                my_another_key: 'my another value'
+            }
+        };
+        fcm.send(message, function(err, res){
+            if (err) {
+                console.log("Something has gone wrong!");
+            } else {
+                console.log("Successfully sent with response: ", res);
+            }
+        });
+        res.status(200).json({result : true});
+      }else{
+        res.status(200).json({result : false, msg:"No Content"});
+      }
+
+    }
+
+  });
+});
+
 module.exports = router;
