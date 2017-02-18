57b5270c3282cacba6f20f264fb27d453f890250
henry <calren00@gmail.com>
2017-02-19 00:31:17 +0900

    schedule settings

--- routes/fcm.js
+++ routes/fcm.js
@@ -6,18 +6,80 @@ var router = express.Router();
 var fcm_config = require('../config/fcm-config.json');
 var serverKey = fcm_config.serverKey;
 var fcm = new FCM(serverKey);
+var async = require('async');
 require('date-utils');
 
 var dt = new Date();
+var d = dt.toFormat('YYYY-MM-DD');
 var h = dt.toFormat('HH24');
-var job = schedule.scheduleJob('0 8,14,19 * * *', function(){
-  if(h == 08){
+
+var job = schedule.scheduleJob('/10 * * * *', function(){
+  console.log("start push");
+  async.waterfall([
+    function(cb){
+      var user=[];
+      db.query('select * from user', function(error,cursor){
+        if(error) console.log(error);
+        for(var i=0;i<cursor.length;i++){
+          if(cursor[i].token){
+            user.push({user:cursor[i].id});
+          }
+        }
+        cb(null,user);
+      });
+    }],
+    function(error,result){
+      if(error)console.log(error);
+      for(var i=0;i<result.length;i++){
+        db.query('select s.user_id, u.id, u.token, s.content, s.starttime, s.date from schedule s inner JOIN user u on s.user_id = ? and u.id=? and date = ? order by starttime asc;',[result[i].id, result[i].id, d], function(error, cursor){
+          if (error){
+            console.log(error);
+          }
+          else {
+            if(cursor.length>0){
+              var contents=[];
+              for(var i=0;i<cursor.length;i++){
+                contents.push({content:cursor[i].content});
+                console.log(contents[i].content);
+              }
+              console.log("************************");
+              var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+                  to: cursor[0].token,
+                  collapse_key: 'schedule',
+
+                  notification: {
+                      title: '오늘 해야 할 일',
+                      body: 'contents'
+                  },
+
+                  data: {  //you can send only notification or only data(or include both)
+                      my_key: contents,
+                      my_another_key: 'my another value'
+                  }
+              };
+              fcm.send(message, function(err, res){
+                  if (err) {
+                      console.log("Something has gone wrong!");
+                  } else {
+                      console.log("Successfully sent with response: ", res);
+                  }
+              });
+            }else{
+              console.log("failed send message");
+            }
+          }
+        });
+      }
+    }
+  );
+  /*
+  if(h == 09){
 
   }else if(h == 14){
 
   }else if(h == 19){
 
-  }
+  }*/
   console.log("send push message");
 })
 
@@ -33,8 +95,6 @@ router.put('/register', function(req, res){
 });
 
 router.get('/push', function(req, res, next){
-  var dt = new Date();
-  var d = dt.toFormat('YYYY-MM-DD');
   db.query('select s.user_id, u.id, u.token, s.content, s.starttime, s.date from schedule s inner JOIN user u on s.user_id = ? and u.id=? and date = ? order by starttime asc;',[req.query.user_id, req.query.id, d], function(error, cursor){
     if (error){
       res.status(500).json({result : error});
